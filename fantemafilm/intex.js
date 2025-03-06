0988d18451d668f9457b93164f2e4543a6622a4c
adipopa15 <150379767+adipopa15@users.noreply.github.com>
2025-03-06 18:17:30 +0100

    Add files via upload

---
+++ fantemafilm/intex.js
@@ -0,0 +1,3 @@
+// Al momento non hai funzionalità JavaScript, ma qui puoi aggiungere funzionalità future!
+// Ad esempio, puoi implementare un carosello, controlli personalizzati per i video o effetti interattivi.
+console.log("Script caricato correttamente!");
